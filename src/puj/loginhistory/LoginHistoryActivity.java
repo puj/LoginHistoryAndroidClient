dde64fa691bd7bb01a0f5ba7ab959b11899f206a
puj <vango85@gmail.com>
2012-03-14 00:52:49 +0100

    Initial cookie storage, encoded password, username, JSON POST to server, settings.

--- src/puj/loginhistory/LoginHistoryActivity.java
+++ src/puj/loginhistory/LoginHistoryActivity.java
@@ -1,14 +1,71 @@
 package puj.loginhistory;
 
+import java.net.URL;
+import java.util.Properties;
+
+import org.apache.http.HttpConnection;
+
+import puj.loginhistory.events.LoginClickListener;
+
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginHistoryActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_area);
-        
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+	}
+
+	@Override
+	public void onStart() {
+		/*
+		 * Choose the correct layout based on three different cases 1. A user
+		 * has never logged in 2. A user does not have a session active 3. A
+		 * user has logged in and has an active session
+		 */
+		SharedPreferences prefs = getApplicationContext().getSharedPreferences(
+				getApplicationContext().getString(
+						R.string.shared_preferences_filename),
+				Context.MODE_PRIVATE);
+		String currentSessionCookie = prefs.getString("sessionCookie", null);
+		boolean isReturningUser = prefs.getBoolean("isReturningUser", false);
+
+		if (currentSessionCookie == null) {
+			setContentView(R.layout.login_area);
+
+			Button submitButton = ((Button) findViewById(R.id.btnSubmit));
+			EditText usernameField = ((EditText) findViewById(R.id.txtUsername));
+			EditText passwordField = ((EditText) findViewById(R.id.txtPassword));
+			submitButton.setOnClickListener(new LoginClickListener(
+					usernameField, passwordField));
+		} else {
+			setContentView(R.layout.main);
+		}
+
+		super.onStart();
+	}
+
+	@Override
+	public void onDestroy() {
+		// Remove the cookie if "remember me" is not checked...
+		SharedPreferences prefs = getApplicationContext().getSharedPreferences(
+				getApplicationContext().getString(
+						R.string.shared_preferences_filename),
+				Context.MODE_PRIVATE);
+
+		Editor prefsEditor = prefs.edit();
+		prefsEditor.remove("sessionCookie");
+		prefsEditor.commit();
+		super.onDestroy();
+	}
+
 }
---
+++ src/puj/loginhistory/events/LoginClickListener.java
@@ -0,0 +1,143 @@
+package puj.loginhistory.events;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import puj.loginhistory.R;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class LoginClickListener implements android.view.View.OnClickListener{
+	/**
+	 * A reference to the text field containing the username
+	 */
+	private EditText mUsernameField;
+	
+	/**
+	 * A reference to the text field containing the password
+	 */
+	private EditText mPasswordField;
+	
+	
+	public LoginClickListener(EditText usernameField, EditText passwordField) {
+		mUsernameField = usernameField;
+		mPasswordField = passwordField;
+	}
+
+	public void onClick(View view) {
+		// Got a login button press
+		if(view.getId() == R.id.btnSubmit){
+			
+			// Construct the target URL
+			final String url = 	"http://" + 
+								view.getContext().getString(R.string.default_login_hostname) + ":" + 
+								view.getContext().getString(R.string.default_server_port) ;
+
+			// Get the username
+			final String username = mUsernameField.getText().toString();
+			
+			// Setup the password encoding arguments
+			final String salt = "09234j234kj!@#213lk$#$)(*)DFSDFL##$";
+			final String passwordAndSalt = username + mPasswordField.getText().toString() + salt;
+			
+			// Encode the password	with base64 and salt
+			String encodedPassword =  null;
+			 try {
+				 	MessageDigest md = MessageDigest.getInstance("SHA-512");
+				 	md.update(passwordAndSalt.getBytes("UTF-8"));
+				 	encodedPassword = new String(md.digest());
+		        } catch (NoSuchAlgorithmException e) {
+		            Log.e("LoginHistory", e.getMessage());
+		        } catch (UnsupportedEncodingException e) {
+		        	Log.e("LoginHistory", e.getMessage());
+				}
+			
+			
+			
+			// Create the JSON credentials
+		    try {
+		    	JSONObject jsonObj = new JSONObject();
+	    	    jsonObj.put("username", username);
+	    	    jsonObj.put("password", encodedPassword);
+
+		    	
+		        // Add your data
+		    	StringEntity entity = new StringEntity(jsonObj.toString(), HTTP.UTF_8);
+		    	entity.setContentType("application/json");
+		    	
+		    	
+		    	// Create a new HttpClient and Post Header
+			    HttpClient httpclient = new DefaultHttpClient();
+			    HttpPost httppost = new HttpPost(url);
+		    	
+		    	httppost.setHeader("Content-Type", "application/json");
+		        httppost.setEntity(entity);
+		        
+		        // Execute HTTP Post Request
+		        HttpResponse response = httpclient.execute(httppost);
+		        	
+		        
+		        // Parse the response
+		        InputStream is = response.getEntity().getContent();
+	            BufferedReader reader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8);
+	            StringBuilder sb = new StringBuilder();
+	            String line = null;
+	            while ((line = reader.readLine()) != null) {
+	                sb.append(line + "\n");
+	            }
+	            is.close();
+		        
+	            
+	            // Store the session cookie
+	            String sessionCookie = sb.toString();
+	            SharedPreferences prefs = view.getContext().getSharedPreferences(
+	            		view.getContext().getString(R.string.shared_preferences_filename), 
+	            		Context.MODE_PRIVATE
+	            		);
+	            
+	            Editor prefsEditor = prefs.edit();
+	            prefsEditor.putString("sessionCookie", sessionCookie);
+	            prefsEditor.commit();
+	            
+	            
+		        Log.i("LogHistory", sb.toString());
+		        
+		    } catch (ClientProtocolException e) {
+		        // TODO Auto-generated catch block
+		    	Log.e("LoginHistory", e.getMessage());
+		    } catch (IOException e) {
+		        // TODO Auto-generated catch block
+		    	Log.e("LoginHistory", e.getMessage());
+		    } catch (JSONException e) {
+				// TODO Auto-generated catch block
+		    	Log.e("LoginHistory", e.getMessage());
+			}
+		}
+	}
+
+}
