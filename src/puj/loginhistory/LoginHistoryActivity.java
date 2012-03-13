4ce1a83b2d89e5b1d52e495f04802d09ecf22816
puj <vango85@gmail.com>
2012-03-13 20:45:24 +0100

    First commit

---
+++ src/puj/loginhistory/LoginHistoryActivity.java
@@ -0,0 +1,14 @@
+package puj.loginhistory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginHistoryActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_area);
+        
+    }
+}
